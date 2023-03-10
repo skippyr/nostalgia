a020c952a3a0e537828296b8221b604f89ac058f
Sherman Rofeman <skippyr.developer@gmail.com>
2023-03-10 17:49:05 -0300

    feat: add scratch of function to change colors

--- Makefile
+++ Makefile
@@ -2,15 +2,21 @@
 name=nostalgia
 comment=A 32x32 cursor for X11 with retro shape
 
+# Characteristics
+inner_color=140000
+border_color=fdebab
+
 # Directories
 repository_directory=$(shell pwd)
 scripts_directory=${repository_directory}/scripts
 configurations_directory=${repository_directory}/configurations
+source_images_directory=${repository_directory}/source_images
+images_directory=${repository_directory}/images
 distribution_directory=${repository_directory}/${name}
 installation_directory=/usr/share/icons/${name}
 
 build:
-	${scripts_directory}/build.sh "${name}" "${comment}" "${configurations_directory}" "${distribution_directory}"
+	${scripts_directory}/build.sh "${name}" "${comment}" "${configurations_directory}" "${source_images_directory}" "${images_directory}" "${distribution_directory}" "${inner_color}" "${border_color}"
 
 install:
 	${scripts_directory}/install.sh "${distribution_directory}" "${installation_directory}"
--- scripts/build.sh
+++ scripts/build.sh
@@ -21,9 +21,29 @@ create_metadata_files() {
   echo -e "[Icon Theme]\nName=${name}\nComment=${comment}\n" > "${distribution_directory}/index.theme"
 }
 
+create_images() {
+  local -r source_images_directory="$1"
+  local -r images_directory="$2"
+  local -r inner_color="$3"
+  local -r border_color="$4"
+
+  local -r source_inner_color=140000
+  local -r source_border_color=fdebab
+
+  utilities::echo_topic_i "Creating images."
+
+  rm -rf "${images_directory}"
+  mkdir -p "${images_directory}"
+
+  for source_image in $(ls "${source_images_directory}"); do
+    convert "${source_images_directory}/${source_image}" -fill "#${inner_color}" -opaque "#${source_inner_color}" "${images_directory}/${source_image}"
+  done
+}
+
 build_cursor_files() {
   local -r configurations_directory="$1"
   local -r distribution_directory="$2"
+  local -r images_directory="$3"
 
   utilities::echo_topic_i "Building cursor files."
 
@@ -34,6 +54,8 @@ build_cursor_files() {
     
     xcursorgen "${configurations_directory}/${configuration_file}" > "${distribution_directory}/cursors/${cursor_file}"
   done
+
+  rm -rf "${images_directory}"
 }
 
 create_symbolic_links() {
@@ -58,14 +80,20 @@ main() {
   local -r name="$1"
   local -r comment="$2"
   local -r configurations_directory="$3"
-  local -r distribution_directory="$4"
+  local -r source_images_directory="$4"
+  local -r images_directory="$5"
+  local -r distribution_directory="$6"
+  local -r inner_color="$7"
+  local -r border_color="$8"
 
   utilities::echo_title "build"
 
-  create_directory_structure "${distribution_directory}"
-  create_metadata_files "${distribution_directory}" "${name}" "${comment}"
-  build_cursor_files "${configurations_directory}" "${distribution_directory}"
-  create_symbolic_links "${distribution_directory}"
+  # create_directory_structure "${distribution_directory}"
+  # create_metadata_files "${distribution_directory}" "${name}" "${comment}"
+  create_images "${source_images_directory}" "${images_directory}" "${inner_color}" "${border_color}"
+  # build_cursor_files "${configurations_directory}" "${distribution_directory}" "${images_directory}"
+  # create_symbolic_links "${distribution_directory}"
 }
 
-main "$1" "$2" "$3" "$4"
+main "$1" "$2" "$3" "$4" "$5" "$6" "$7"
+
